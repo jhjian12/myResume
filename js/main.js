b24d9af497eeea59f4975d2a0824552b1fc445a1
jhjian <jeffjian0612@gmail.com>
2019-02-01 14:53:04 +0800

    set main.js

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is M.Reza", "I'm a Web Designer", "Love Simplicity"],
+        strings: ["My Name is Jeff", "I'm a Master Student at NCCU", "Love Programming","Glad to meet you"],
         typeSpeed: 100,
         backDelay: 900,
         // loop
