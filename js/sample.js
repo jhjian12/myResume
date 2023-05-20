a2e3add9809325cea2637564ddfde349f970ab76
Jeff Jian <jeff.jian@appier.com>
2023-05-21 01:58:07 +0800

    revise something wrong

--- js/sample.js
+++ js/sample.js
@@ -1,5 +1,6 @@
 <script type="application/javascript">
-function sayHi(){
-    console.log('said hi')
-}
+    document.getElementById("idftbz7fvy3hqgnbs").innerHTML = "hihi";
+    function sayHi(){
+        console.log('said hi')
+    }
 </script>
